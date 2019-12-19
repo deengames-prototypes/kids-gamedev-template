25a57f00226943e1e78e30871368c2e1ba51d797
nightblade9 <nightbladecodes@gmail.com>
2019-12-19 18:30:09 -0500

    Character spins

---
+++ Khalid/Entities/Player.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _process(delta):
+	$Sprite.rotation_degrees += 1
