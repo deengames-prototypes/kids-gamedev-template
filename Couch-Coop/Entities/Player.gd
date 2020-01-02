f79d16cbacfeb6951b17e711194244f56ae5310f
nightblade9 <nightbladecodes@gmail.com>
2020-01-02 15:44:31 -0500

    Colour unique per spinner Remove device-agnostic input keys

--- Couch-Coop/Entities/Player.gd
+++ Couch-Coop/Entities/Player.gd
@@ -1,7 +1,24 @@
+tool
 extends KinematicBody2D
 
 const ROTATION_VELOCITY = 360
 const TURBO_MULTIPLIER = 2
+
+export var joypad_id = 0
+export var tint:Color setget set_tint, get_tint
+
+var _tint:Color
+
+func set_tint(value):
+	if value != null and $Sprite != null:
+		_tint = value
+		$Sprite.modulate = _tint
+
+func get_tint():
+	return _tint
+	
+func _ready():
+	$CollisionShape2D.visible = true
 
 func _process(delta):
 	var rotation = (ROTATION_VELOCITY * delta)
