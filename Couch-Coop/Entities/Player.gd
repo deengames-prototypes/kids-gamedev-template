5336e53a8e359f900ee2bfbe0748aa4e6276ea74
nightblade9 <nightbladecodes@gmail.com>
2020-01-02 15:16:02 -0500

    Turbo works

--- Couch-Coop/Entities/Player.gd
+++ Couch-Coop/Entities/Player.gd
@@ -1,6 +1,13 @@
 extends KinematicBody2D
 
-const ROTATION_VELOCITY = 720
+const ROTATION_VELOCITY = 360
+const TURBO_MULTIPLIER = 2
 
 func _process(delta):
-	self.rotation_degrees += (ROTATION_VELOCITY * delta)
+	var rotation = (ROTATION_VELOCITY * delta)
+	
+	if Input.is_action_pressed("joypad_snes_y"):
+		rotation *= TURBO_MULTIPLIER
+	
+	self.rotation_degrees += rotation
+	
