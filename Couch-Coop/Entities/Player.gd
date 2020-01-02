5c86cef0495ac457d25a98cee5c931197d2d4b4c
nightblade9 <nightbladecodes@gmail.com>
2020-01-02 15:57:23 -0500

    Manual event processing for joypad events

--- Couch-Coop/Entities/Player.gd
+++ Couch-Coop/Entities/Player.gd
@@ -1,13 +1,21 @@
 tool
 extends KinematicBody2D
 
+const MOVE_SPEED = 200
 const ROTATION_VELOCITY = 360
 const TURBO_MULTIPLIER = 2
+
+const UP_BUTTON_ID = 12
+const DOWN_BUTTON_ID = UP_BUTTON_ID + 1
+const LEFT_BUTTON_ID = DOWN_BUTTON_ID + 1
+const RIGHT_BUTTON_ID = LEFT_BUTTON_ID + 1
 
 export var joypad_id = 0
 export var tint:Color setget set_tint, get_tint
 
+var _velocity:Vector2 = Vector2.ZERO
 var _tint:Color
+var _turbo:bool = false
 
 func set_tint(value):
 	if value != null and $Sprite != null:
@@ -23,8 +31,26 @@ func _ready():
 func _process(delta):
 	var rotation = (ROTATION_VELOCITY * delta)
 	
-	if Input.is_action_pressed("joypad_snes_y"):
+	if _turbo:
 		rotation *= TURBO_MULTIPLIER
 	
 	self.rotation_degrees += rotation
 	
+	self.move_and_slide(_velocity * MOVE_SPEED)
+	
+
+func process_joypad(event):
+		_velocity = Vector2(0, 0)
+		var button = event as InputEventJoypadButton
+		
+		if button.button_index == UP_BUTTON_ID:
+			_velocity.y = -1
+		elif button.button_index == DOWN_BUTTON_ID:
+			_velocity.y = 1
+			
+		if button.button_index == LEFT_BUTTON_ID:
+			_velocity.x = -1
+		elif button.button_index == RIGHT_BUTTON_ID:
+			_velocity.x = 1
+		
+		
--- Couch-Coop/Main.gd
+++ Couch-Coop/Main.gd
@@ -1,6 +1,24 @@
 extends Node2D
 
-func _unhandled_input(event):
+var _current_event:InputEventJoypadButton
+var _player_joypad_map = {}
+
+func _ready():
+	var children = self.get_children()
+	for child in children:
+		if "Player" in child.name:
+			_player_joypad_map[child.joypad_id] = child
+
+func _unhandled_input(event):	
 	if event is InputEventJoypadButton:
-		var button = event as InputEventJoypadButton
-		print("Controller " + str(button.device) + " pressed " + str(button.button_index))
+		print(str(event.device) + " => " + str(event.button_index))
+		if event.is_pressed():
+			_current_event = event
+		else:
+			_current_event = null # released
+			
+func _process(delta):
+	if _current_event != null:
+		var joypad_id = _current_event.device
+		if joypad_id in _player_joypad_map:
+			_player_joypad_map[joypad_id].process_joypad(_current_event)
